55ea4264f4f02a972457246b5a837fc5364d7adc
Ahmadhan01 <ahmadrafdy@students.amikom.ac.id>
2026-06-10 12:25:43 +0700

    Initial commit - Nicky Frozen

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import { bunny } from 'laravel-vite-plugin/fonts';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react'; // ← tambah ini
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.jsx'], // ← .js → .jsx
+            refresh: true,
+            fonts: [
+                bunny('Instrument Sans', {
+                    weights: [400, 500, 600],
+                }),
+            ],
+        }),
+        tailwindcss(),
+        react(), // ← tambah ini
+    ],
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
+});
